e21a8673955ce75b650cdc7af159ef89d281f838
Steph <sweetcactusflower@gmail.com>
2022-05-20 18:55:48 -0400

    Added right-sided fold-out hamburger menu w/ animated hamburger icon (layout and styling) to index.html and style.css. Added hamburger menu variables w/ event listener function in index.js.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// Hamburger Menu
+const hamMenuBtn = document.getElementById('ham-menu-btn');
+const hamMenuItems = document.getElementById('ham-menu-items');
+
+hamMenuBtn.addEventListener('click', () => {
+  hamMenuBtn.classList.toggle('open');
+  hamMenuItems.classList.toggle('unfold');
+  hamMenuItems.classList.toggle('close');
+});
+
+
+// animated arrow, onclick to main
+function toMain() {
+  let scrollToTransformText = document.getElementById('firstText');
+  scrollToTransformText.scrollIntoView();
+}
+
+
 // Intersection Observer API for back-to-top button, visibility on scroll and animation via clasList.add
 const target = document.querySelector('.topBtn');
 function handleIntersection(entries) {
@@ -17,11 +35,4 @@ observer.observe(target);
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-}
-
-
-// animated arrow, onclick to main
-function toMain() {
-  let scrollToTransformText = document.getElementById('firstText');
-  scrollToTransformText.scrollIntoView();
 }
